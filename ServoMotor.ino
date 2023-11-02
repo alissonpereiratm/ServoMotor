2984fbd03309f33fbeb283779bcdf0b6ef98b3a6
Alisson <alissonpereiratm@gmail.com>
2023-11-01 21:38:25 -0300

    Servo

--- ServoMotor.ino
+++ ServoMotor.ino
@@ -1,27 +1,69 @@
+#include <Stepper.h>
 #include "Servo.h"
+#include "Ultrasonic.h" //INCLUSÃO DA BIBLIOTECA NECESSÁRIA PARA FUNCIONAMENTO DO CÓDIGO
 
+const int echoPin = 2; //PINO DIGITAL UTILIZADO PELO HC-SR04 ECHO(RECEBE)
+const int trigPin = 3; //PINO DIGITAL UTILIZADO PELO HC-SR04 TRIG(ENVIA)
+
+Ultrasonic ultrasonic(trigPin,echoPin); //INICIALIZANDO OS PINOS DO ARDUINO
+
+int distancia; //VARIÁVEL DO TIPO INTEIRO
+String result; //VARIÁVEL DO TIPO STRING
 Servo meuservo1;
 Servo meuservo2;
 int angulo1 = 0; 
 int angulo2 = 0; 
-int incremento = 5; 
+int incremento = 5;
+
+const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
+// for your motor
+
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
 
 void setup() {
-  meuservo1.attach(9); 
-  meuservo2.attach(10); 
+
+    pinMode(echoPin, INPUT); //DEFINE O PINO COMO ENTRADA (RECEBE)
+  pinMode(trigPin, OUTPUT); //DEFINE O PINO COMO SAIDA (ENVIA)
+  Serial.begin(9600); //INICIALIZA A PORTA SERIAL
+  
+
+  meuservo1.attach(6); 
+  // meuservo2.attach();
+  // set the speed at 60 rpm:
+  myStepper.setSpeed(5);
+  // initialize the serial port:
+  Serial.begin(9600);
 }
 
 void loop() {
-  while (angulo1 <= 90) {
-    meuservo2.write(angulo2);
-    meuservo1.write(angulo1);
-    delay(500); 
-    angulo1 += incremento;
-      }
-  if (angulo1 >= 90) {
-    meuservo2.write(angulo2);
-     delay(500); 
-    angulo2 += incremento; 
-    angulo1 = 0;
-     }
+for ( int y = 0; y <= 90; y+=incremento){
+    meuservo1.write(y);
+    for(int x = 0; x <= stepsPerRevolution; x++){
+    myStepper.step(1);
+      hcsr04(); // FAZ A CHAMADA DO MÉTODO "hcsr04()"
+  Serial.print("Distancia "); //IMPRIME O TEXTO NO MONITOR SERIAL
+  
+  Serial.print(result); ////IMPRIME NO MONITOR SERIAL A DISTÂNCIA MEDIDA
+  Serial.println("cm"); //IMPRIME O TEXTO NO MONITOR SERIAL
+    }
+  
 }
+}
+//MÉTODO RESPONSÁVEL POR CALCULAR A DISTÂNCIA
+void hcsr04(){
+    digitalWrite(trigPin, LOW); //SETA O PINO 6 COM UM PULSO BAIXO "LOW"
+    delayMicroseconds(2); //INTERVALO DE 2 MICROSSEGUNDOS
+    digitalWrite(trigPin, HIGH); //SETA O PINO 6 COM PULSO ALTO "HIGH"
+    delayMicroseconds(10); //INTERVALO DE 10 MICROSSEGUNDOS
+    digitalWrite(trigPin, LOW); //SETA O PINO 6 COM PULSO BAIXO "LOW" NOVAMENTE
+    //FUNÇÃO RANGING, FAZ A CONVERSÃO DO TEMPO DE
+    //RESPOSTA DO ECHO EM CENTIMETROS, E ARMAZENA
+    //NA VARIAVEL "distancia"
+    distancia = (ultrasonic.read(CM)); //VARIÁVEL GLOBAL RECEBE O VALOR DA DISTÂNCIA MEDIDA
+    result = String(distancia); //VARIÁVEL GLOBAL DO TIPO STRING RECEBE A DISTÂNCIA(CONVERTIDO DE INTEIRO PARA STRING)
+ }
+
+
+
+
